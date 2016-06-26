6a8476452e80f3a88d6f5dac4132a075f0ebbc2d
Jeffrey Erlich <jerlich@erlichlab.org>
2016-06-26 16:20:37 +0800

    clean up code. - replace string concat with sprintf for speed and readability

--- +db/getSessData.m
+++ +db/getSessData.m
@@ -18,8 +18,8 @@ function [S, extra_args]=getSessData(varargin)
 % cell array of date string of the from "YYYY-MM-DD"
 %
 % pairs={'do_tracking' false;...
-% 	   'fetch_peh' true...
-% 	   };
+%      'fetch_peh' true...
+%      };
 %
 %
 
@@ -34,7 +34,7 @@ if nargin==0 || isempty(varargin{1})
     return;
 end
 
-dbc = db.labdb.getConnection;
+dbc = db.labdb.getConnection();
 
 if iscell(varargin{1})
     varargin=varargin{1};
@@ -80,7 +80,7 @@ end
 extra_args=varargin;
 
 do_tracking=false;
-fetch_peh=true;
+fetch_peh=true;  % The PEH can be large, sometimes we don't need it.
 utils.overridedefaults({'fetch_peh'}, extra_args);
 
 %% get data from sql
@@ -91,7 +91,7 @@ if ~use_sessid
     % string.
     datestr='';
     for dx=1:numel(dates)
-        datestr=[datestr ',"'  dates{dx}  '"'];
+        datestr=sprintf('%s , "%s"', datestr, dates{dx});
     end
     % Use the datestr for a select ... where sessiondate in (datestr) type sql command to get all the relevant sessions.
     sqlquery = sprintf('select sessid from beh.sessions b, met.subjects m where m.subjid=b.subjid and subjname=%d and sessiondate in ( %s ) order by sessiondate',subjname, datestr(2:end));
@@ -105,20 +105,15 @@ end
 % We have a list of sessids.  Transform that into a comma separated string
 sessstr='';
 for sx=1:numel(sessid)
-    sessstr=[sessstr, ',' num2str(sessid(sx))];
+    sessstr=sprintf('%s, %d', sessstr, sessid(sx));
 end
 sessstr = sessstr(2:end);
 if fetch_peh
-    
-    sqlquery = 	['select sessid, trialnum, data, parsed_events from beh.trials where sessid in (' sessstr ') order by sessid, trialnum'];
-    
+    sqlquery = sprintf('select sessid, trialnum, data, parsed_events from beh.trials where sessid in ( %s ) order by sessid, trialnum', sessstr);
     trialsout = dbc.query(sqlquery);
-    
 else
-    sqlquery = 	['select sessid, trialnum, data from beh.trials where sessid in (' sessstr ') order by sessid, trialnum'];
-    
+    sqlquery = sprintf('select sessid, trialnum, data from beh.trials where sessid in ( %s ) order by sessid, trialnum', sessstr);
     trialsout = dbc.query(sqlquery);
-    
 end
 
 sqlquery = sprintf('select sessid, sessiondate, starttime, hostip, protocol from beh.sessions where sessid in ( %s ) order by sessid', sessstr);
@@ -129,111 +124,115 @@ S = combineData(sessout, trialsout, fetch_peh);
 
 if false && do_tracking % This is not implemented yet.
     S.a=cell(numel(S.sessid),1);
-    
+
     [T.sessid, T.ts, T.theta]=bdata(['select sessid, ts, theta from tracking where sessid in (' sessstr ')']);
-    
+
     for sx=1:numel(S.sessid)
-        
+
         tx=find(T.sessid==S.sessid(sx));
         if ~isempty(tx)
             a.ts=T.ts{tx};
             a.theta=T.theta{tx};
-            
+
             S.a{sx}=a(:);
         end
     end
-    
-    
-    
+
+
+
 end
 end
 
 function S = combineData(sessout, trialsout, fetch_peh)
 
 
-if which('utils.fromjson')
-	ljson = @utils.fromjson;
-    fastjson = true;
-else
-    ljson = @loadjson;
-    fastjson = false;
-end
+    if which('utils.fromjson')
+        ljson = @utils.fromjson;
+        fastjson = true;
+    else
+        ljson = @loadjson;
+        fastjson = false;
+    end
 % This let's people with the faster json code use it while the slow pokes are stuck with the other one.
 
-	
-tic
-S = table2struct(sessout);
-for sx = 1:numel(S)
-    these_trial_ind = trialsout.sessid == S(sx).sessid;
-    num_trials = sum(these_trial_ind);
-    
-    %% First handle the trial data.
-    these_json_data = trialsout.data(these_trial_ind);
-    last_data = ljson(these_json_data{num_trials});
-    clear sessdata
-    sessdata(num_trials) = last_data.data;
-    sessdata = sessdata(:); 
-    % The above two lines help allocate memory for the struct at once to
-    % save time. And then make sure it is a column vector (e.g. tall).
-    
-    for tx = 1:(num_trials-1)
-        this_data = ljson(these_json_data{tx});
-        if fastjson
-            this_data=convert2mat(this_data);
+    S = table2struct(sessout);
+    for sx = 1:numel(S)
+        these_trial_ind = trialsout.sessid == S(sx).sessid;
+        num_trials = sum(these_trial_ind);
+
+        %% First handle the trial data.
+        these_json_data = trialsout.data(these_trial_ind);
+        clear sessdata
+        % By going backwards we allocate memory for the struct at once to
+        % save time.
+        for tx = num_trials:-1:1
+            this_data = ljson(these_json_data{tx});
+            sessdata(tx) = this_data.data;      
         end
-        sessdata(tx) = this_data.data;      
-    end
 
-    S(sx).data = sessdata;
+        S(sx).data = sessdata(:);
 
-    %% Then handle the parsed events if we got it.
+        %% Then handle the parsed events if we got it.
 
-    if fetch_peh
-	    these_json_pe = trialsout.parsed_events(these_trial_ind);
-    	last_data = ljson(these_json_pe{num_trials});
-	    clear sessdata;
-	    sessdata(num_trials) = last_data.parsed_events;
-	    sessdata = sessdata(:); 
-	    % The above two lines help allocate memory for the struct at once to
-	    % save time. And then make sure it is a column vector (e.g. tall).
-	    
-	    for tx = 1:(num_trials-1)
-	        this_data = ljson(these_json_pe{tx});
-            if fastjson
-                this_data=convert2mat(this_data);
+        if fetch_peh
+            these_json_pe = trialsout.parsed_events(these_trial_ind);
+            clear sessdata;
+            % By going backwards we allocate memory for the struct at once to
+            % save time. 
+            for tx = num_trials:-1:1
+                this_data = ljson(these_json_pe{tx});
+                if fastjson
+                    this_data.parsed_events=convert2mat(this_data.parsed_events);
+                end
+                sessdata(tx) = this_data.parsed_events;      
             end
-	        sessdata(tx) = this_data.parsed_events;      
-	    end
+            S(sx).peh = sessdata(:);
+        end
+    end
 
-	    S(sx).peh = sessdata;
+end
 
+function x = convert2mat(x)
+    if isfield(x,'States')
+        try
+            field = fieldnames(x.States);
+            for fx = 1:numel(field)
+                x.States.(field{fx}) = reccell2mat(x.States.(field{fx}));
+            end
+
+            field = fieldnames(x.Events);
+            for fx = 1:numel(field)
+                tt = x.Events.(field{fx});
+                if iscell(tt)
+                    x.States.(field{fx}) = cell2mat(tt);
+                end
+            end    
+        catch
+        end
     end
 
 
+    function tt = reccell2mat(tt)
 
-end
-toc
-end
-
-function y = convert2mat(x)
-if isfield(x,'States')
-    try
-    field = fieldnames(x.States);
-    for fx = 1:numel(field)
-        x.States.(field{fx}) = cell2mat(x.States.(field{fx}));
+        if iscell(tt{1})
+            if fx == 1
+                tt{1}{1} = 0;
+                % matlab thinks 0 is an int64
+            end
+            tt = cell2mat([tt{:}]');
+        elseif ischar(tt)
+            if ~strcmpi(tt,'_nan__nan_')
+                
+                fprintf(1,'Cannot handle %s',tt);
+                warning('db.getSessData:reccell2mat','Converted string to [NaN NaN]')
+            end
+            tt = [nan nan];
+        else
+        % this is the case where there was only one entry into the state.
+            if fx == 1
+                tt{1} = 0;
+            end
+        tt = cell2mat(tt');
+        end
     end
-        
-        field = fieldnames(x.Events);
-    for fx = 1:numel(field)
-        x.States.(field{fx}) = cell2mat(x.Events.(field{fx}));
-    end    
-catch
-    end
-end
-    
-    
-end
-
-
-
-
+end % convert2mat
