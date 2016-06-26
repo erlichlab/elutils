a64298417c16c1efb43dc0c50d6e7c346bc7bf53
Jeffrey Erlich <jerlich@erlichlab.org>
2016-06-26 16:27:25 +0800

    some early dev on a SoundManager to make it easier to generate sounds for protocols and also a Subjects class

---
+++ +utils/Subject.m
@@ -0,0 +1,20 @@
+classdef Subject < handle
+
+properties
+	name
+	id
+	species
+	experimenters
+	mass
+	stage	
+	sessions
+end
+
+properties (SetAccess=private, GetAccess=private)
+end
+
+
+methods
+end
+
+end
