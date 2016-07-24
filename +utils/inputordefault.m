26d88b96d718fdee65023a41d28ed39e57e62d6b
Jeffrey Erlich <jerlich@erlichlab.org>
2016-07-24 13:27:04 +0800

    remove the found keywords from the list and return shrunken list.

--- +utils/inputordefault.m
+++ +utils/inputordefault.m
@@ -1,4 +1,4 @@
-function out = inputordefault(keyname, defval, inps)
+function [out, inps] = inputordefault(keyname, defval, inps)
 % out = inputordefault(keyname, defval, inps)
 % Parses a cell array (usually varargin) looking for a string which matches 
 % the keyname and then assigns out to the value of the next element of the
@@ -9,6 +9,7 @@ function out = inputordefault(keyname, defval, inps)
  % is the keyname in the inputs? Look only at the odd elements of inps, since this keyname could be a value of another input.
  if any(ind)
  	out = inps{2*find(ind)};
+ 	inps(2*find(ind)-1:2*find(ind)) = [];  % erase these from the list.
  else
  	out = defval;
  end
