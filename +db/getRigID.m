9050f1f0b535f5b1e856aa77a90d218027bd468c
Jeffrey Erlich <jerlich@nyu.edu>
2016-07-29 14:13:19 +0800

    function to get rigID

--- +db/getRigID.m
+++ +db/getRigID.m
@@ -2,4 +2,5 @@ function id = getRigID()
 
 dbc = db.labdb.getConnection();
 
-id = dbc.get('select rigid from met.rigs where ipaddr="%s"',db.get_network_info());
+id = dbc.get('select rigid from met.rigs where ipaddr="%s"',{db.get_ip()});
+
