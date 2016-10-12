23458bae197784c4ec5e06e0fb49f77af692130e
Jeffrey Erlich <jerlich@nyu.edu>
2016-10-12 18:29:57 +0800

    new file enforce_range

---
+++ +utils/enforce_range.m
@@ -0,0 +1,5 @@
+function val = enforce_range(val ,minval, maxval)
+% val = enfore_range(val, minval, maxval)
+% ensures that the value of x is in between minval and maxval
+% val, minval, maxval can be vectors or the same length or some can be scalar
+	val = min(maxval, max(minval, val));
