b183db96c822a8d76ba6da535751c05403de9a2b
Jeffrey Erlich <jerlich@erlichlab.org>
2017-02-06 11:36:12 +0800

    if target is empty return list

--- +utils/rm_from_list.m
+++ +utils/rm_from_list.m
@@ -1,7 +1,10 @@
 function out = rm_from_list(carr, target)
 % list = rm_from_list(list, item)
 % Finds the string item in the list and removes it.
-
+    if isempty(target)
+        out = carr;
+        return
+    end
     cind = strcmp(target, carr);
     out = carr;
     out(cind) = [];
