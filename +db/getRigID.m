616caf0d57aa703e4761fbd93313f621d47ff386
Jeffrey Erlich <jerlich@erlichlab.org>
2017-02-11 14:57:09 +0800

    remove bpod specific functions

--- +db/getRigID.m
+++
@@ -1,9 +0,0 @@
-function [id, roomid] = getRigID()
-
-dbc = db.labdb.getConnection();
-
-[id, roomid] = dbc.get('select rigid, roomid from met.rigs where ipaddr="%s"',{db.get_ip()});
-
-if isempty(id)
-	id = 0;
-end
