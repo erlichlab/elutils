f7c2cf0a38b6a6dedfe5beacf33db735356ffc3e
Jeff Erlich <jerlich@nyu.edu>
2018-11-03 11:26:06 -0700

    plot pokes in same order as getPokeList

--- +draw/plot_poke_wall.m
+++ +draw/plot_poke_wall.m
@@ -1,14 +1,15 @@
-function ax = plot_poke_wall(varargin)
+function [ax,x,y,names] = plot_poke_wall(varargin)
 
     iod = @utils.inputordefault;
     [ax, varargin] = iod('ax',[],varargin);
-    marker = iod('Marker','o',varargin);
-    MarkerSize = iod('MarkerSize',10,varargin);
+    marker = iod('Marker','s',varargin);
+    MarkerSize = iod('MarkerSize',50,varargin); 
     
     
     row_height = 0.8;
-    x = [-2 -1 -1  0 0 1  1 2];
-    y = [0   row_height -row_height -row_height*1.5 0 row_height -row_height 0];
+    x = [2 1 1  0  -1  -1 -2 0];
+    y = [0 row_height -row_height  0 row_height -row_height 0 -row_height*1.5];
+    names = getPokeList();
     
     if isempty(ax)
         ax = draw.jaxes();
@@ -18,5 +19,6 @@ function ax = plot_poke_wall(varargin)
     h.Marker = marker;
     h.LineStyle = 'none';
     h.MarkerSize = MarkerSize;
-    keyboard
+    h.Color = 'k';
+    
 end
