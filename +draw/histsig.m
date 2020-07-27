94a225029365e1663ab91544fd479032dbdd22fd
Jeffrey Erlich <jerlich@nyu.edu>
2020-07-27 16:21:03 +0800

    almost

--- +draw/histsig.m
+++ +draw/histsig.m
@@ -1,64 +1,64 @@
 
 
-function [ hx]=histsig(x, x_sig, x_lim)
+function [ hx]=histsig(x, x_sig, varargin)
 % [ hx]=histsig(x, x_sig,  x_lim)
 
-org=0.1;
-wdth=0.2;
-hist_h=0.180;
-num_bns=17;
+
+inpd = @utils.inputordefault;
+origin=inpd('origin', 0.1, varargin);
+wdth=inpd('width',0.2,varargin);
+hist_h=inpd('height',0.180,varargin);
+num_bins=inpd('n_bins',17,varargin);
+bins=inpd('bins',[],varargin);
+ax=inpd('ax',[],varargin);
+x_lim=inpd('x_lim',[],varargin);
+zero=inpd('zero',0,varargin);
 
 gd=~isnan(x);
 x=x(gd);
 x_sig=x_sig(gd);
+x_lim_b=[min(x)-0.1 max(x)+0.1];
 
-	x_lim_b=[min(x)-0.1 max(x)+0.1];
-
-if nargin==2;
+if isempty(x_lim)
 	x_lim=x_lim_b;
 end
 	
 
 
-figure
-
-hx=axes('Position',[org org wdth hist_h]);
+if isempty(ax)
+    ax=draw.jaxes('Position',[origin origin wdth hist_h]);
+end
 
 marker_size=zeros(size(x))+12;
 marker_size(x_sig==1)=24;
 
 % make the x-axis histogram
-
-axes(hx);
-bns=linspace(x_lim_b(1),x_lim_b(2),num_bns);
-nsig=histc(x(x_sig==0), bns);
-nsig=nsig(1:end-1);
-sig=histc(x(x_sig==1), bns);
-sig=sig(1:end-1);
+if isempty(bins)
+    bins=linspace(x_lim_b(1),x_lim_b(2),num_bins);
+end
+nsig=histcounts(x(x_sig==0), bins);
+sig=histcounts(x(x_sig==1), bins);
 maxy=max(nsig(:)+sig(:))*1.3;
-cbns=edge2cen(bns);
-[hh]=bar(cbns, [sig(:) nsig(:)],'stacked');
-xlim(x_lim);
-set(gca, 'YAxisLocation','left');
+cbins=edge2cen(bins);
+[hh]=bar(ax,cbins, [sig(:) nsig(:)],'stacked');
+xlim(ax,x_lim);
+set(ax, 'YAxisLocation','left');
 set(hh(1),'FaceColor','k')
 set(hh(2),'FaceColor',[1 1 1])
-set(gca,'box','off','YLim',[0 maxy])
-set(gca,'Color','none')
+set(ax,'box','off','YLim',[0 maxy])
+set(ax,'Color','none')
 text(getx,gety,[num2str(round(100*mean(x_sig))) '% p<0.05'])
 
 x_mean=nanmean(x);
-[xt_sig,xt_mu,B]=stats.bootmean(x);
+[xt_sig,~,B]=stats.bootmean(x-zero);
 
-[CI]=prctile(B,[2.5 97.5]);
-
-x_se=stats.nanstderr(x);
+[CI]=prctile(B+zero,[2.5 97.5]);
 
 if xt_sig<0.05
-    y_lim=ylim(hx);
+    y_lim=ylim(ax);
     y_lim=y_lim(2);
-    hold(hx, 'on')
-    plot(hx,[x_mean],[0.9*y_lim],'.k','MarkerSize',6);
-    plot(hx,[CI(1) CI(2)], [0.9*y_lim 0.9*y_lim], '-k');
+    plot(ax,x_mean,0.9*y_lim,'.k','MarkerSize',6);
+    plot(ax,[CI(1) CI(2)], [0.9*y_lim 0.9*y_lim], '-k');
     
 end
  
