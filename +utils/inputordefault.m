e4ad573298956460f9114a1c8adb92b1f0fd6b9f
Jeffrey Erlich <jerlich@nyu.edu>
2020-11-16 16:49:17 +0800

    convert struct to names list.

--- +utils/inputordefault.m
+++ +utils/inputordefault.m
@@ -5,7 +5,19 @@ function [out, inps] = inputordefault(keyname, defval, inps)
  % input if it finds it otherwise assignes it to the default value.
  % E.g. sessid = inputordefault('sessid', 0, varargin)
 % Can also be called on the leftovers to check that there were no missing arguments.
+% Can also be called to convert a struct into a list of 
  if nargin==1
+	if isstruct(keyname)
+		% convert to name, value
+		fn = fieldnames(keyname);
+		out = cell(1,2*numel(fn));
+		for fx = 1:numel(fn)
+			out{2*fx-1} = fn{fx};
+			out{2*fx} = keyname.(fn{fx});
+		end
+
+		return
+	end
 	if ~isempty(keyname)
 		warning('You have unused keyword arguments')
 		for argx = 1:2:numel(keyname)
