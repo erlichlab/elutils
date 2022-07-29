45586a0b0b785f79ff165bf43a52ae1ba29a06b4
Jingjie Li <jingjie.li.21@ucl.ac.uk>
2022-07-29 17:08:11 +0100

    debugging

--- +net/zmqhelper.m
+++ +net/zmqhelper.m
@@ -68,7 +68,7 @@ classdef zmqhelper < handle
         end
         
         function [addr, out] = recvjson(obj)
-            msg = recvmsg(obj); % get msg with nonblocking and convert from java string to char
+            msg = obj.recvmsg(); % get msg with nonblocking and convert from java string to char
             if length(recvmsg)>6
                 [addr, jstr] = strtok(msg, ' ');  % split the message into address and json string
                 out = jsondecode(jstr);   % decode the json string and return the address and the json object
